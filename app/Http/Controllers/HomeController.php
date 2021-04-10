86b094eb10bd3fe705b526a64a80e2ec7fa17ee6
Houcem Hedhly <hedhoucem@gmail.com>
2021-04-10 11:30:16 +0100

    requesting data from db

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Contact;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function welcome()
     {
-        return view('welcome');
+        // $contacts = Contact::all();
+        // return view('welcome', ['contacts' => $contacts]);
+        // ou bien
+        return view('welcome', ['contacts' => Contact::all()]);
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -84,16 +84,11 @@
                     DSI2 - ISET Bizerte
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                <ul>
+                    @foreach ($contacts as $contact)
+                        <li>{{ $contact['name'] }} ({{ $contact['phone'] }}) {{ $contact['address'] }}</li>
+                    @endforeach
+                </ul>
             </div>
         </div>
     </body>
