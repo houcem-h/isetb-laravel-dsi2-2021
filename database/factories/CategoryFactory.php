1b597960a1ac8f3b92536ac511fe68e0ef85adbc
Houcem Hedhly <hedhoucem@gmail.com>
2021-04-24 11:16:26 +0100

    creating category's factory and seeder

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Category;
+use Faker\Generator as Faker;
+
+$factory->define(Category::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word,
+        'description' => $faker->sentence,
+        'created_at' => now()
+    ];
+});
