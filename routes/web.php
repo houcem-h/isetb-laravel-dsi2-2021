c5617de3304f2bc3b025e4754d3e2f0fd1cc6526
Houcem Hedhly <hedhoucem@gmail.com>
2021-05-08 10:41:16 +0100

    redirect user when auth based on role

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,6 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/admin', function () {
+Route::get('/admin-dashboard', function () {
     return view('admin.dashboard');
 });
