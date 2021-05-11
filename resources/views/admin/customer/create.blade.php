001af8c5b0d5a36da4d4ea0a97425ca3e1208c98
Houcem Hedhly <hedhoucem@gmail.com>
2021-05-11 17:59:07 +0100

    validation rules and store in db

--- resources/views/admin/customer/create.blade.php
+++ resources/views/admin/customer/create.blade.php
@@ -9,13 +9,15 @@
                 <div class="col">
                     <div class="form-group">
                         <label for="firstname">Firstname</label>
-                        <input type="text" name="firstname" id="firstname" class="form-control" placeholder="Firstname goes here">
+                        <input type="text" name="firstname" value="{{ old('firstname') }}" id="firstname" class="form-control @error('firstname') is-invalid @enderror @error('lastname') is-invalid @enderror" placeholder="Firstname goes here">
+                        @error('firstname')<div class="text-danger">{{ $message }}</div>@enderror
                       </div>
                 </div>
                 <div class="col">
                     <div class="form-group">
                         <label for="lastname">Lastname</label>
-                        <input type="text" name="lastname" id="lastname" class="form-control" placeholder="Lastname goes here">
+                        <input type="text" name="lastname" value="{{ old('lastname') }}" id="lastname" class="form-control @error('lastname') is-invalid @enderror" placeholder="Lastname goes here">
+                        @error('lastname')<div class="text-danger">{{ $message }}</div>@enderror
                       </div>
                 </div>
             </div>
@@ -23,19 +25,22 @@
                 <div class="col">
                     <div class="form-group">
                         <label for="phone">Phone</label>
-                        <input type="text" name="phone" id="phone" class="form-control" placeholder="+21612345678">
+                        <input type="text" name="phone" value="{{ old('phone') }}" id="phone" class="form-control @error('phone') is-invalid @enderror" placeholder="+21612345678">
+                        @error('phone')<div class="text-danger">{{ $message }}</div>@enderror
                       </div>
                 </div>
                 <div class="col">
                     <div class="form-group">
                         <label for="email">Email</label>
-                        <input type="email" name="email" id="email" class="form-control" placeholder="person@example.com">
+                        <input type="email" name="email" value="{{ old('email') }}" id="email" class="form-control @error('email') is-invalid @enderror" placeholder="person@example.com">
+                        @error('email')<div class="text-danger">{{ $message }}</div>@enderror
                       </div>
                 </div>
             </div>
             <div class="form-group">
               <label for="address">Address</label>
-              <input type="text" name="address" id="address" class="form-control" placeholder="Address goes here">
+              <input type="text" name="address" value="{{ old('address') }}" id="address" class="form-control @error('address') is-invalid @enderror" placeholder="Address goes here">
+              @error('address')<div class="text-danger">{{ $message }}</div>@enderror
             </div>
             <div class="row">
                 <div class="col"><button type="submit" class="btn btn-block btn-outline-primary"><i class="fas fa-save"></i> Save</button></div>
